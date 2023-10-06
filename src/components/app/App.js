271ece64f0c7cb76f1332f8b648c5fe627a2a692
jennycherny <evchern.it@gmail.com>
2023-10-06 15:05:16 +0400

    added error boundary

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -22,12 +23,18 @@ class App extends Component {
         <div className="app">
             <AppHeader/>
             <main>
-                <RandomChar/>
-                <div className="char__content">
+                <ErrorBoundary>
+                    <RandomChar/>
+                </ErrorBoundary>
+            <div className="char__content">
+                <ErrorBoundary>
                     <CharList onCharSelected={this.onCharSelected}/>
+                </ErrorBoundary>
+                <ErrorBoundary>
                     <CharInfo charId={this.state.selectedChar}/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
     )
